308234e8656448e44a2ca429fc246ce0180b3210
tomoyuki65 <tomoyuki65@users.noreply.github.com>
2025-02-20 03:04:16 +0900

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# kindでk8sを試すサンプル  
+kindでローカルにk8sの開発環境を構築し、  
+Go言語のAPIをデプロイするサンプルです。  
+  
+## 機能一覧  
+・テキスト表示１（GET /）  
+・テキスト表示２（GET /k8s-go）
+  
+## 使用技術  
+Go "1.24"  
+Echo "v4.13.3"  
+Docker  
+docker-compose  
+kubectl  
+kind  
+k8s  
+  
+## 参考記事  
+[・kindでKubernetes（k8s）のローカル開発環境を作ってGo言語（Golang）のAPIをデプロイする方法](https://golang.tomoyuki65.com/how-to-build-a-k8s-development-environment-with-kind)  
+  
---
+++ src/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "net/http"
+    "github.com/labstack/echo/v4"
+)
+
+func main() {
+    e := echo.New()
+
+    e.GET("/", func(c echo.Context) error {
+        return c.String(http.StatusOK, "Hello, World !!")
+    })
+
+    e.GET("/k8s-go", func(c echo.Context) error {
+        return c.String(http.StatusOK, "Go API on k8s with kind !!")
+    })
+
+    e.Logger.Fatal(e.Start(":8080"))
+}
